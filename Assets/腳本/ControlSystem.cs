4cc2606afb10a59a4f9140a040cfd473e3aafbe8
john20000220 <59871467+john20000220@users.noreply.github.com>
2023-05-14 00:56:32 +0800

    新增動畫、攝影機、怪物生成

---
+++ Assets/腳本/ControlSystem.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class ControlSystem : MonoBehaviour
+{
+    [Header("移動速度")]
+    [Range(0.5f, 99.9f)]
+    public float moveSpeed = 3.5f;
+
+    [Header("2D 剛體")]
+    public Rigidbody2D rig;
+
+    [Header("動畫控制器")]
+    public Animator ani;
+
+    [Header("跑步參數")]
+    public string parRun = "開關被打";
+
+    private void Awake()
+    {
+        print("起床囉");
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        MoveAndAnimation();
+    }
+
+    private void MoveAndAnimation()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rig.velocity = new Vector3(h, v, 0) * moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        ani.SetBool(parRun, h > 0 || h < 0 || v > 0 || v < 0);
+    }
+}
---
+++ Assets/腳本/SpawnSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SpawnSystem : MonoBehaviour
+{
+    [Header("生成間隔")]
+    public float interval = 3.5f;
+
+    [Header("怪物預製物")]
+    public GameObject prefabEnemy;
+
+    private void SpawnEnemy()
+    {
+        Instantiate(prefabEnemy, transform.position, transform.rotation);
+    }
+
+    private void Awake()
+    {
+        InvokeRepeating("SpawnEnemy", 0, interval);
+    }
+}
